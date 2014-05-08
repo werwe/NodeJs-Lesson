c1f8f4a702330b3e747ca816c7880be455f021b3
werwe <werwe.me@gamil.com>
2014-05-08 16:59:12 +0900

    formidable module을 이용한 multipart upload 구현

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,23 @@
 var querystring = require("querystring");
+var fs = require("fs");
+var formidable = require("formidable");
+var util = require("util");
 
 function start(response) {
 	console.log("Request handler 'start ' was called");
 	var body = '<html>' +
 		'<head>' +
-		'<meta http-equiv="Content-Ttype" content="text/html;" ' +
+		'<meta http-equiv="Content-Type" content="text/html; ' +
 		'charset=UTF-8" />' +
 		'</head>' +
-		'<form action="/upload" method="post">' +
-		'<textarea name="text" rows="20" cols="60"></textarea>' +
-		'<input type="submit" value="Submit text" />' +
+		'<body>' +
+		'<form action="/upload" enctype="multipart/form-data" ' +
+		'method="post">' +
+		'<input type="file" name="upload" multiple="multiple">' +
+		'<input type="submit" value="Upload file" />' +
 		'</form>' +
 		'</body>' +
 		'</html>';
-
 	response.writeHead(200, {
 		"Content-Type": "text/html"
 	});
@@ -21,14 +25,40 @@ function start(response) {
 	response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called");
-	response.writeHead(200, {
-		"Content-Type": "text/plain"
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files) {
+		fs.renameSync(files.upload.path, "tmp/test.png");
+		response.writeHead(200, {
+			"Content-Type": "text/HTML"
+		});
+		response.write("recieved image:<br/>");
+		response.write("<img src='/show' />");
+		response.end(util.inspect({fields:fields,files:files}));
+		console.log("parsing done");
 	});
-	response.write("You've sent:" + postData + querystring.parse(postData).text);
-	response.end();
 }
 
+function show(response) {
+	console.log("Request handler 'show' was called");
+	fs.readFile("tmp/test.png", "binary", function(error, file) {
+		if (error) {
+			response.writeHead(500, {
+				"Content-Type": "text/plain"
+			});
+			response.write(error + "/n");
+			response.end();
+		} else {
+			response.writeHead(200, {
+				"Content-Type": "image/png"
+			});
+			response.write(file, "binary");
+			response.end();
+		}
+	});
+}
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
